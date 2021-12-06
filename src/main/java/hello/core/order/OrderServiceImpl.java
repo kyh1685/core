b2d95b388a376afe3f70ce38e003cee86052b782
kyh <qnvnfls@naver.com>
2021-12-06 13:23:54 +0900

    2021-12-6 ~ 관심사의 분리

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,14 +2,24 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // 인터페이스에만 의존해야 하는데 구현체에도 의지함(DIP 위반)
+    // 기능을 확장하거나 변경했을 때 클라이언트 코드에 영향을 줌(OCP 위반)
+    /*private final DiscountPolicy discountPolicy = new FixDiscountPolicy();*/
+    /*private final DiscountPolicy discountPolicy = new RateDiscountPolicy();*/
+    private final DiscountPolicy discountPolicy;
+    private final MemberRepository memberRepository;
+
+    public OrderServiceImpl(DiscountPolicy discountPolicy, MemberRepository memberRepository) {
+        this.discountPolicy = discountPolicy;
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
